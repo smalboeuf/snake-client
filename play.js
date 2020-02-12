34c75672d3e85e2832305724a185f35ffd90072f
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-02-12 18:27:51 +0000

    Added ways to take inputs

--- play.js
+++ play.js
@@ -1,4 +1,36 @@
 const { connect } = require('./client');
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
 
+
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = () => {
+
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+  
+    });
+  };
+
+  stdin.on('data', (handleUserInput));
+  return stdin;
+}
+
+
+
+
+
+
+setupInput();
 console.log('Connecting ...');
 connect();
